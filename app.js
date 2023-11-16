df5066776ce00a0a7f74ba4080f36900dbdb101d
Rabin Pun <rabin.pun@ebpearls.com>
2023-11-16 18:44:51 +0545

    asd

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ res.sendFile(path.join(__dirname, 'public', 'wallet.html'));
 
 app.get('/apple-app-site-association', (req, res) => {
   res.setHeader("Content-Type", "application/json")
-  res.sendFile(path.join(__dirname, '', 'wallet.html'));
+  res.sendFile(path.join(__dirname, '' , 'apple-app-site-association'));
   
   });
 
